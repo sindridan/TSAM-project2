faaa2d00b08c305105b1ddf0f322e1d297e00531
Sindri Dan Gardarsson <sindridan@gmail.com>
2019-09-11 15:57:24 +0000

    some changes from internet

--- client.cpp
+++ client.cpp
@@ -19,8 +19,8 @@ int main(int argc, char const *argv[]) {
     string ipAddress = argv[1]; //"130.208.243.61";
     int portNoLow = atoi(argv[2]); //from 4000
     int portNoHigh = atoi(argv[3]); //to 4100
-    int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
+    // Slide packet 1
     fd_set readfds, masterfds;
     // IBM Code
     int bytes_sent;
@@ -32,34 +32,50 @@ int main(int argc, char const *argv[]) {
 
     struct timeval timeout;
 
-    timeout.tv_sec = 1;
-    timeout.tv_usec = 0;    
+    
     
     to.sin_family = AF_INET;
     inet_pton(AF_INET, ipAddress.c_str(), &to.sin_addr);
 
     for(int i = portNoLow; i <= portNoHigh; i++) {
+        timeout.tv_sec = 1;
+        timeout.tv_usec = 0;    
+        int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
         memset(&to, 0, sizeof(to));
+        
+        FD_ZERO(&masterfds);
+        FD_SET(sock, &masterfds);
+        
         to.sin_port = htons(i);
         cout << "Port no: " << i << endl;
-        
-        bind(sock, (struct sockaddr*)&to, sizeof(to));
-        //IBM code
-        cout << "Sending bytes to " << i << ".." << endl;
-        bytes_sent = sendto(sock, data_sent, sizeof(data_sent), 0, (struct sockaddr*)&to, sizeof(to));
-        socklen_t addrlen = sizeof(from);
-        cout << "Size of addrlen " << addrlen << ".." << endl;
-        cout << "Now receiving bytes from " << i << ".." << endl;
-        
-        
-        bytes_received = recvfrom(sock, data_recv, sizeof(data_recv), 0, &from, &addrlen);
-        cout << "Bytes received from " << i << ".." << endl;
 
-        if (sizeof(data_recv) > 0) {
-            cout << "Port no: " << i << " sent back data!";
-        } else {
-            cout << "Port no: " << i << " sent no data back.....";
+        bind(sock, (struct sockaddr*)&to, sizeof(to));
+        bytes_sent = sendto(sock, data_sent, sizeof(data_sent), 0, (struct sockaddr*)&to, sizeof(to));
+        if(select(sock+1, &readfds, NULL, NULL, &timeout) < 0)
+        {
+            cout << "Error on select";
         }
+        cout << "Inbetween select and FD_ISSET" << endl;
+
+        if(FD_ISSET(sock, &readfds))
+        {
+            cout << "Read from socket";
+            //IBM code
+            cout << "Sending bytes to " << i << ".." << endl;
+            socklen_t addrlen = sizeof(from);
+            cout << "Size of addrlen " << addrlen << ".." << endl;
+            cout << "Now receiving bytes from " << i << ".." << endl;
+            bytes_received = recvfrom(sock, data_recv, sizeof(data_recv), 0, &from, &addrlen);
+            cout << "Bytes received from " << i << ".." << endl;
+
+            if (sizeof(data_recv) > 0) {
+            cout << "Port no: " << i << " sent back data!";
+            } else {
+            cout << "Port no: " << i << " sent no data back.....";
+            }
+        }
+        close(sock);
+        cout << "Closed socket" << endl;
     }
     
     return 0;
