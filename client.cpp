4dcbee964685f1ab1b457c14e2909a28664e7dbc
Sindri Dan Gardarsson <sindridan@gmail.com>
2019-09-13 14:22:40 +0000

    integrated header

--- client.cpp
+++ client.cpp
@@ -15,6 +15,7 @@ Code referenced:
 https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.1.0/com.ibm.zos.v2r1.hala001/sendto.htm
 https://www.binarytides.com/raw-udp-sockets-c-linux/
 https://www.digi.com/resources/documentation/Digidocs/90002002/Tasks/t_calculate_checksum.htm?TocPath=API%20Operation%7CAPI%20frame%20format%7C_____1
+https://gist.github.com/listnukira/4045436 
 */
 
 using namespace std;
@@ -66,11 +67,11 @@ int main(int argc, char const *argv[]) {
     // IBM Code
     int bytes_sent;
     int bytes_received;
-    char data_sent[256]; // Used for raw socket
+    char data_sent[256], *pseudogram; // Used for raw socket
     string hello = "SHOW ME WHAT YOU GOT";
     char *udpData;
     char data_recv[256];
-    struct sockaddr_in to;
+    struct sockaddr_in to, my_addr;
     struct sockaddr from;
     struct pseudo_header pHeader;
     vector<int> openPorts;
@@ -113,6 +114,17 @@ int main(int argc, char const *argv[]) {
         }
         
     }
+    
+    // Get our address and socket
+
+    char myIP[16];
+    unsigned int myPort;
+
+    bzero(&my_addr, sizeof(my_addr));
+	socklen_t len = sizeof(my_addr);
+	getsockname(sock, (struct sockaddr *) &my_addr, &len);
+	inet_ntop(AF_INET, &my_addr.sin_addr, myIP, sizeof(myIP));
+	myPort = ntohs(my_addr.sin_port);
     //close the socket after use
     close(sock);
 
@@ -129,14 +141,55 @@ int main(int argc, char const *argv[]) {
 
     // Fill in UDP header 
     struct udphdr *udpHeader = (struct udphdr*) (data_sent + sizeof(struct ip));
+
+    struct pseudo_header psh;
     udpData = data_sent + sizeof(struct iphdr) + sizeof(struct udphdr); // TODO: comment
     strcpy(udpData, "knock");
 
+    //some address resolution
+    
 
-    //Continue here....
+    //to.sin_port = htons();
+    //to.sin_addr.s_addr = inet_addr ("192.168.1.1");
+     
+    //Fill in the IP Header
+    ipHeader->ihl = 5;
+    ipHeader->version = 4;
+    ipHeader->tos = 0;
+    ipHeader->tot_len = sizeof (struct iphdr) + sizeof (struct udphdr) + strlen(udpData);
+    ipHeader->id = htonl (54321); //Id of this packet
+    ipHeader->frag_off = 0;
+    ipHeader->ttl = 255;
+    ipHeader->protocol = IPPROTO_UDP;
+    ipHeader->check = 0;      //Set to 0 before calculating checksum
+    ipHeader->saddr = inet_addr ( myIP );    //Spoof the source ip address
+    ipHeader->daddr = to.sin_addr.s_addr;
+     
+    //Ip checksum
+    ipHeader->check = chkSum ((unsigned short *) udpData, ipHeader->tot_len);
+     
+    //UDP header
+    udpHeader->source = htons (6666);
+    udpHeader->dest = htons (8622);
+    udpHeader->len = htons(8 + strlen(udpData)); //tcp header size
+    udpHeader->check = 0; //leave checksum 0 now, filled later by pseudo header
+     
+    //Now the UDP checksum using the pseudo header
+    psh.source_address = inet_addr( ipAddress.c_str() );
+    psh.dest_address = to.sin_addr.s_addr;
+    psh.zeroes = 0;
+    psh.protocol = IPPROTO_UDP;
+    psh.udp_length = htons(sizeof(struct udphdr) + strlen(udpData) );
+     
+    int psize = sizeof(struct pseudo_header) + sizeof(struct udphdr) + strlen(udpData);
+    pseudogram = (char*)malloc(psize);
+     
+    memcpy(pseudogram , (char*) &psh , sizeof (struct pseudo_header));
+    memcpy(pseudogram + sizeof(struct pseudo_header) , udpHeader , sizeof(struct udphdr) + strlen(udpData));
+     
+    udpHeader->check = chkSum( (unsigned short*) pseudogram , psize);
 
 
-    to.sin_port = htons(4041);
     memset(&to, 0, sizeof(to));
     memset(data_recv, 0, sizeof(data_recv));
     inet_pton(AF_INET, ipAddress.c_str(), &to.sin_addr);
